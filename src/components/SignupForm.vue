ddf5f622feffb8dac0729435d3afb1537a60ace6
Rodrigo Lins <rlinsdev@gmail.com>
2021-07-16 21:39:42 -0300

    Check box

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -11,9 +11,15 @@
       <option value="developer">Web Developer</option>
       <option value="designer">Web Designer</option>
     </select>
-    <p>{{email}}</p>
-    <p>{{password}}</p>
+    
+    <div class="term">
+      <input type="checkbox" required v-model="terms" />
+      <label>Accept Terms and conditions</label>
+    </div>
+
   </form>
+  <p>{{email}}</p>
+  <p>{{password}}</p>
 </template>
 
 <script>
@@ -22,7 +28,8 @@ export default {
     return {
       email: '',
       password:'',
-      role:''
+      role:'',
+      terms: false
     }
   }
 
@@ -55,5 +62,12 @@ input, select {
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+input[type="checkbox"]{
+  display: inline-block;
+  width: 16px;
+  margin:0 10px 0 0;
+  position: relative;
+  top: 2px
 }
 </style>
