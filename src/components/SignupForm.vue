79621ddbb9b8ba191002dd25250e501131e5e764
Rodrigo Lins <rlinsdev@gmail.com>
2021-07-17 20:44:28 -0300

    validation

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email:</label>
     <input type="email" required v-model="email">
 
     <label>Password:</label>
     <input type="password" required v-model="password">
+    <div v-if="passwordError" class="error">{{passwordError}}</div>
 
     <label>Role:</label>
     <select v-model="role">
@@ -21,6 +22,10 @@
     <div class="terms">
       <input type="checkbox" required v-model="terms" />
       <label>Accept Terms and conditions</label>
+    </div>
+
+    <div class="submit">
+      <button>Create an Account</button>
     </div>
     
   </form>
@@ -40,7 +45,8 @@ export default {
       role:'',
       terms: false,
       tempSkills: '',
-      skills:[]
+      skills:[],
+      passwordError:''
     }
   },
   methods:{
@@ -57,6 +63,11 @@ export default {
       this.skills = this.skills.filter((item)=>{
         return skill!==item
       })
+    },
+    handleSubmit(){
+
+      this.passwordError = this.password.length > 5 ? 
+        '' : 'Password must be at least 6 charactere'
     }
   }
 }
@@ -107,5 +118,22 @@ input[type="checkbox"]{
   font-weight: bold;
   color: #777;
   cursor: pointer;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+.submit{
+  text-align: center;
+}
+.error {
+  color:#ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
 }
 </style>
